78ca40949b42af786d887eaf08b5403c61ca061a
Tobias Windisch <tobias.windisch@posteo.de>
2019-08-08 07:58:29 +0200

    use json

--- uphone/config.py
+++ uphone/config.py
@@ -1,11 +1,15 @@
-import ujson
+
+try:
+    import ujson as json
+except ImportError:
+    import json
 
 
 class Config(object):
 
     def __init__(self, filepath):
         self.filepath = filepath
-        self._config = ujson.load(open(filepath))
+        self._config = json.load(open(filepath, 'r'))
 
     def get_wifi_credentials(self):
         return self._config["wifi"]["ssid"], self._config["wifi"]["key"]
