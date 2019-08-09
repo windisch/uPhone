621058c52e4006dae2cd413f0272c244cc733077
Tobias Windisch <tobias.windisch@posteo.de>
2019-08-09 20:16:07 +0200

    add installation routine for micropython

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 from setuptools import setup
+import sys
+
+
+if sys.implementation.name == 'micropython':
+    import sdist_upip
+    cmdclass={'sdist': sdist_upip.sdist},
+    install_requires = [
+        "micropython-logging"
+    ]
+
+else:
+
+    cmdclass={}
+    install_requires = []
 
 setup(
     name="uphone",
@@ -6,8 +20,7 @@ setup(
     packages=["uphone"],
 
     # dependencies
-    install_requires=[
-    ],
+    install_requires=install_requires,
     tests_require=[
         "pytest",
     ],
@@ -17,6 +30,7 @@ setup(
     author_email="tobias.windisch@posteo.de",
     description="A baby phone running on MicroPython",
     license="GNU GPL3",
+    cmdclass=cmdclass,
     keywords="baby phone micropyhton",
     url="https://github.com/windisch/uPhone",
 )
